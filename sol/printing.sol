034f84744a0daef99368a022b7c0706796cc77c7
Dennis Hezel <dh@3yourmind.com>
2018-02-21 20:20:00 +0100

    Printing cont

--- sol/printing.sol
+++ sol/printing.sol
@@ -11,9 +11,6 @@ contract Printing {
     }
 
     struct PrintingService {
-        string name;
-        string company;
-        string addr;
         address account;
         uint numParts;
         mapping(uint => Part) parts;
@@ -27,17 +24,14 @@ contract Printing {
         string uuid;
     }
 
-    struct Customer {
+    mapping(address => PrintingService) printingServices; //printing service address -> PrintingService
+    mapping(string => Part) printableParts; //uuid -> Part
+    address adrCustomer;
+    address adrOem;
+
+    function Printing() {
+        adrOem = msg.sender;
     }
-
-    //struct PrintingLicense {
-    //}
-
-    OEM oem;
-    //PrintingLicense public printingLicense;
-    Customer customer;
-    mapping(address => PrintingService) printingServices;
-    mapping(string => Part) printableParts;
 
     function giveRightToPrint(address adrOfPrintingService, string uuidOfPart) public {
         uint index = getIndexOfPart(msg.sender, uuidOfPart);
@@ -47,14 +41,40 @@ contract Printing {
         printingService.parts[printingService.numParts++] = printableParts[uuidOfPart];
     }
 
+    function registerSenderAsPrintingService() public {
+        PrintingService service;
+        service.account = msg.sender;
+        printingServices[msg.sender] = service;
+    }
+
+    function addPart(string _gcodeUrl, uint _price, string _material, string _uuid) public {
+        if (msg.sender != adrOem) return;
+        printableParts[_uuid].gcodeUrl = _gcodeUrl;
+        printableParts[_uuid].price = _price;
+        printableParts[_uuid].material = _material;
+        printableParts[_uuid].owner = msg.sender;
+    }
+
     function print(string uuidOfPart) public returns (string gcodeUrl, uint price, string material) {
         uint index = getIndexOfPart(msg.sender, uuidOfPart);
+        if (index == printingServices[msg.sender].numParts) return; // part is not on the list of parts that the msg.sender is allowed to print
         var part = printingServices[msg.sender].parts[index];
-        if (msg.sender != part.owner) return;
         gcodeUrl = part.gcodeUrl;
         price = part.price;
         material = part.material;
         delete printingServices[msg.sender].parts[index];
+        msg.sender.transfer(price * 4 / 5); // printing service gets 80% of the money
+        part.owner.transfer(price * 1 / 5); // OEM gets 20%
+    }
+
+    function getPrice(string uuidOfPart) public returns (uint price) {
+        price = printableParts[uuidOfPart].price;
+    }
+
+    function orderPrint(string uuidOfPart) public payable returns (bool) {
+        if (msg.value != printableParts[uuidOfPart].price) return false; // customer does not have enough money
+        adrCustomer = msg.sender;
+        return true;
     }
 
     function getIndexOfPart(address adrOfPrintingService, string uuidOfPart) view returns (uint i) {
